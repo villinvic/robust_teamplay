65f47da8788886c07fd068183cc736e65c7e30fc
villinvic <vvillin@hotmail.fr>
2024-02-06 17:09:51 +0100

    bob

--- random_mdp_experiment.py
+++ random_mdp_experiment.py
@@ -85,7 +85,7 @@ def main(policy_lr, prior_lr, n_seeds=1, episode_length=10, pop_size=2, n_steps=
         # ),
     ]
 
-    lr_samples = np.logspace(-8, -4, 10)
+    lr_samples = np.logspace(-8, -2, 10)
 
     approaches = [
         dict(
