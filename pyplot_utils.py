fa5971d4b1a87c88cb6052d4cef4bcfcc96323bd
villinvic <vvillin@hotmail.fr>
2024-02-07 14:09:35 +0100

    bob

--- pyplot_utils.py
+++ pyplot_utils.py
@@ -18,7 +18,7 @@ def make_grouped_boxplot(data, name="grouped_boxplot", whiskers=(0, 100)):
 
     plt.figure()
 
-    colors = ['lightblue', 'lightgreen', 'lightcoral', 'lightskyblue', 'gray', 'lightgray']
+    colors = ['lightblue', 'lightgreen', 'lightcoral', 'lightskyblue', 'lightgray', 'gray']
 
     num_runs = len(data)
 
