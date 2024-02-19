2a88f2fca75115d44422108f45d0d17b91953296
villinvic <vvillin@hotmail.fr>
2024-02-19 15:23:39 +0100

    selfplay ?

--- random_mdp_experiment.py
+++ random_mdp_experiment.py
@@ -308,7 +308,7 @@ def random_mdp_experiment(
         if len(policy_history) > 3 :
             policy_history.pop(0)
         print(policy_history)
-        previous_robust_policy = np.random.choice(policy_history)
+        previous_robust_policy = policy_history[np.random.randint(0, len(policy_history))]
 
         expected_vf, vf = algo.policy_evaluation_for_prior(bg_population, belief)
 
