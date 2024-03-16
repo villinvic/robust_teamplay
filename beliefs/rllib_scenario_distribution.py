11d362482a40ebfef28c56ae9f287a253cefc2e2
villinvic <vvillin@hotmail.fr>
2024-03-16 11:11:00 +0100

    bf_sgda

--- beliefs/rllib_scenario_distribution.py
+++ beliefs/rllib_scenario_distribution.py
@@ -294,6 +294,23 @@ class ScenarioMapper:
         return policy_id
 
 
+def _compile_iteration_results(
+    self, *, episodes_this_iter, step_ctx, iteration_results=None
+):
+    r = self._compile_iteration_results(self, episodes_this_iter=episodes_this_iter, step_ctx=step_ctx,
+                                    iteration_results=iteration_results)
+
+    scenario_counts = defaultdict(int)
+    for episode in episodes_this_iter:
+        for k in episode.custom_metrics:
+            if "utility" in k:
+                scenario_counts[k.rstrip("_utility")] += 1
+
+    r["custom_metrics"]["scenario_counts"] = scenario_counts
+
+    return r
+
+
 class ScenarioDistribution:
 
     def __init__(self, algo: Algorithm, learn_best_responses=False):
@@ -335,23 +352,6 @@ class ScenarioDistribution:
                 self.current_best_response_scenario = self.missing_best_responses.popleft()
 
             self.set_matchmaking()
-
-        def _compile_iteration_results(
-        algo, *, episodes_this_iter, step_ctx, iteration_results=None
-    ):
-            r = algo._compile_iteration_results(algo, episodes_this_iter=episodes_this_iter, step_ctx=step_ctx,
-                                            iteration_results=iteration_results)
-
-            scenario_counts = defaultdict(int)
-            for episode in episodes_this_iter:
-                for k in episode.custom_metrics:
-                    if "utility" in k:
-                        scenario_counts[k.rstrip("_utility")] += 1
-
-            r["custom_metrics"]["scenario_counts"] = scenario_counts
-
-            return r
-
 
         self.algo._compile_iteration_results = _compile_iteration_results.__get__(self.algo, type(self.algo))
 
