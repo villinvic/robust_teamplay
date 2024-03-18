e20b13f68f8945bb91dae96e0aca22bd097daf1a
villinvic <vvillin@hotmail.fr>
2024-03-18 11:31:09 +0100

    bf_sgda

--- rllib_experiments/learn_best_responses.py
+++ rllib_experiments/learn_best_responses.py
@@ -113,7 +113,7 @@ def main(
         lambda_=0.95,
         gamma=1.,
         entropy_coeff=0.,
-        lr=1e-2,
+        lr=1e-3,
         use_critic=True,
         use_gae=True,
         #kl_coeff=0.,
