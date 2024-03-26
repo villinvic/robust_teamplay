1f537d93e7c05bcc2ec91a1ad5246057fb8ba9e7
villinvic <vvillin@hotmail.fr>
2024-03-26 14:03:21 +0100

    no history test

--- rllib_experiments/bf_sgda.py
+++ rllib_experiments/bf_sgda.py
@@ -90,9 +90,12 @@ def main(
     for policy_id in (Scenario.MAIN_POLICY_ID, Scenario.MAIN_POLICY_COPY_ID):
         policies[policy_id] = (None, dummy_env.observation_space[0], dummy_env.action_space[0], {})
 
+    batch_size = rollout_fragment_length * num_workers
+    max_samples = 50_000_000
+    num_iters = max_samples // batch_size
     config = make_bf_sgda_config(ImpalaConfig).training(
         beta_lr=beta_lr, #2e-1,
-        beta_smoothing=10_000,
+        beta_smoothing=int(num_iters * 0.1),
         use_utility=use_utility,
         scenarios=scenarios,
         copy_weights_freq=1,
@@ -103,7 +106,7 @@ def main(
         # IMPALA
         # opt_type="rmsprop",
         entropy_coeff=1e-4,
-        train_batch_size=rollout_fragment_length * num_workers ,
+        train_batch_size=batch_size,
         momentum=0.,
         epsilon=1e-5,
         decay=0.99,
@@ -175,7 +178,7 @@ def main(
         checkpoint_freq=300,
         keep_checkpoints_num=3,
         stop={
-            "timesteps_total": 50_000_000,
+            "timesteps_total": max_samples,
         },
     )
 
