5e06731e37698e91452271de5659a7d3e7666942
villinvic <vvillin@hotmail.fr>
2024-03-27 15:52:19 +0100

    learn background policies

--- rllib_experiments/learn_best_responses.py
+++ rllib_experiments/learn_best_responses.py
@@ -59,9 +59,6 @@ def main(
         background_population=["background_" + p.name for p in background_population]
     )
 
-    print(scenarios.scenario_list)
-    exit()
-
     register_env(env_id, env_config.get_maker(num_scenarios=len(scenarios)))
 
     rollout_fragment_length = env_config.episode_length // 10
